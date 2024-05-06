6474eb2459e9cce5837bd03f1633eb71b241aa09
MD JASIM UDDIN <mjunachole97@gmail.com>
2024-05-06 15:48:24 +0600

    customer about us message sections added

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -97,8 +97,8 @@ const HomePage = () => {
             </div>
 
 
-            <div>
-                <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 bg-gray-100 text-gray-800">
+            <div className='my-10'>
+                <div className="grid w-full grid-cols-1 gap-8 py-16 rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 bg-gray-100 text-gray-800">
                     <div className="flex flex-col justify-between">
                         <div className="space-y-2">
                             <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
